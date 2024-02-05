e1be7baf4013e8814d4c31e28b13bf129624a063
Sean Bunch <databaws@gmail.com>
2024-02-05 12:02:10 -0500

    file structure update

---
+++ src/components/algo.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Algo() {
+
+    return(
+        <div>
+            <h1>
+            Bible Algo
+            </h1>
+        </div>
+    )
+}
+
+export default Algo;
